4f27d0bdb7768db97c66e5e5a38d06c75d2e07f6
Tree-Nulbo <exopas58@gmail.com>
2019-04-11 13:33:58 +0900

    main frame updated

--- gooddat/main.py
+++ gooddat/main.py
@@ -9,14 +9,19 @@ from db import *
 import crawler 
 import categorize
 
-# DB 읽어오기
-urlLink = dataDownload()
+def main():
+    # DB 읽어오기
+    urlLink = dataDownload()
 
-# Crawler에 URL 넘겨주기 및 작동
-crawlResult = crawler.driver(urlLink)
+    # Crawler에 URL 넘겨주기 및 작동
+    crawlResult = crawler.driver(urlLink)
 
-# Algorithm에 결과 넘기기
-categResult = categorize.tokenize(crawlResult)
+    # Algorithm에 결과 넘기기
+    categResult = categorize.tokenize(crawlResult)
 
-# 최종 결과값 DB에 저장하기
-dataUpload(categResult)
+    # 최종 결과값 DB에 저장하기
+    dataUpload(categResult)
+
+if __name__ == '__main__':
+    main()
+    
