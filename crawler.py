8d8ed52336f9ef9c83b74034eeecfe48070dff28
Donghoon Han <dhk1349@naver.com>
2019-04-17 02:38:34 +0900

    demo version

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
+from math import ceil
 
 def driver(urlLink):
     print("URL transmission complete.")
@@ -15,10 +16,22 @@ def flyday(url):
         -url 받으면 댓글 몇 개 있는지 확인하고 몇 번 돌릴지 확인하는 함수.
         -이에 따라 page=의 인덱스 찾아 url내용 바꿔주는 코드
     '''
+    urls=urlopen("http://widgets2.cre.ma/flyday.co.kr/products/reviews?app=0&iframe=1&iframe_id=crema-product-reviews-1&page=1&parent_url=http%3A%2F%2Fwww.flyday.co.kr%2Fshop%2Fshopdetail.html%3Fbranduid%3D1115007%26xcode%3D018%26mcode%3D001%26scode%3D%26sort%3Dorder%26cur_code%3D018001%26ref%3Dwmobi&product_code=1115007&widget_env=100")
+    obj=soup(urls,"html.parser")
+    pages=obj.find('div',{"id":"content"}).find('div',{"class":"products_reviews_list"}).find('div',{"class":"widget_reviews"}).find('div',{"class":"products_reviews_header"}).find('div',{"class":"products_reviews_header__upper"}).find('span',{"class":"products_reviews_header__sort_type products_reviews_header__sort_type--selected"}).find('span',{"class":"reviews-count"}).text
+    temp=""
+    for i in range(0,len(pages)):
+        if(pages[i].isdigit()):
+            temp+=pages[i]
+    pages=ceil(int(temp)/5)
+
+    urlfront=url[:url.find("page=")+5]
+    urlback=url[url.find("&parent"):]
+    
     textfile=open('comment.txt','w')
-    for i in range(1,249):
-        url=urlopen("http://widgets2.cre.ma/flyday.co.kr/products/reviews?app=0&iframe=1&iframe_id=crema-product-reviews-1&page="+str(i)+"&parent_url=http%3A%2F%2Fwww.flyday.co.kr%2Fshop%2Fshopdetail.html%3Fbranduid%3D1115007%26xcode%3D018%26mcode%3D001%26scode%3D%26sort%3Dorder%26cur_code%3D018001%26ref%3Dwmobi&product_code=1115007&widget_env=100")
-        obj=soup(url,"html.parser")
+    for i in range(1,pages+1):
+        urls=urlopen(urlfront+str(i)+urlback)
+        obj=soup(urls,"html.parser")
         
         comm=obj.find('div',{"id":"content"}).find('div',{"class":"products_reviews_list"}).find('div',{"class":"widget_reviews"}).find('div',{"class":"widget_reviews__body products_reviews_list__body"}).find('div',{"class":"page"}).find('ul',{"class":"reviews reviews-product"}).find_all('li',{"class":"review products_reviews_list_review product_review__container "})
         
